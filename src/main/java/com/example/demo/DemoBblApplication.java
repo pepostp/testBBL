01964ee2392d309edb2d51dce4e195010292ecd0
Suthipong K <suthipong.khattiya@gmail.com>
2024-05-29 17:43:44 +0700

    [BE] : Start new project

---
+++ src/main/java/com/example/demo/DemoBblApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBblApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBblApplication.class, args);
+	}
+
+}
